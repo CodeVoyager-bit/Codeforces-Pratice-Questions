bd0e7c0e70baf6f8f7b8ee47cc472c5e5708f523
Krish Modi <krishmodi3030@gmail.com>
2025-02-28 15:59:36 +0530

    Update C. Everybody Likes Good Arrays!.py

--- C. Everybody Likes Good Arrays!.py
+++ C. Everybody Likes Good Arrays!.py
@@ -6,7 +6,6 @@
 # ) are of different parity. Note that an array of size 1 is trivially good.
 # You are given an array of size 𝑛.
 # In one operation you can select any pair of adjacent elements in which both elements are of the same parity, delete them, and insert their product in the same position.
-
 # Find the minimum number of operations to form a good array.
 
 # Input
