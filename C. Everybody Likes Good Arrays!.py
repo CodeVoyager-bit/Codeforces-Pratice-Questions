679c83a3a17a1bfa19c2680557b99c621bef1120
Krish Modi <krishmodi3030@gmail.com>
2025-03-03 22:39:13 +0530

    Update C. Everybody Likes Good Arrays!.py

--- C. Everybody Likes Good Arrays!.py
+++ C. Everybody Likes Good Arrays!.py
@@ -10,7 +10,6 @@
 # Input
 # Each test contains multiple test cases. The first line contains the number of test cases 𝑡
 #  (1≤𝑡≤500). The description of the test cases follows.
-
 # The first line of each test case contains an integer 𝑛(1≤𝑛≤100).
 
 # The second line of each test case contains 𝑛 integers 𝑎1,𝑎2,…,𝑎𝑛 (1≤𝑎𝑖≤10^9).
