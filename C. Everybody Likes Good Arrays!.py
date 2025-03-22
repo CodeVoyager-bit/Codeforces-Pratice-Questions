2eac5d857ac2e87b29e71ac22dd2b6f43c8967ee
Krish Modi <krishmodi3030@gmail.com>
2025-03-22 20:09:12 +0530

    Update C. Everybody Likes Good Arrays!.py

--- C. Everybody Likes Good Arrays!.py
+++ C. Everybody Likes Good Arrays!.py
@@ -29,8 +29,7 @@
 # 3
 
 # Note
-# Consider the first test case. Select the 2-nd and the 3-rd integers and apply the operation on them. The array changes from [1,7,11,2,13] to [1,77,2,13]
-# . Next, select the 1-st and the 2-nd integers, array changes from [1,77,2,13] to [77,2,13]. Thus we require 2 operations. It can be proved that this is the minimum number of operations.
+# Consider the first test case. Select the 2-nd and the 3-rd integers and apply the operation on them. The array changes from [1,7,11,2,13] to [1,77,2,13]. Next, select the 1-st and the 2-nd integers, array changes from [1,77,2,13] to [77,2,13]. Thus we require 2 operations. It can be proved that this is the minimum number of operations.
 
 # In the second test case, the given array is already good. So we require 0 operations.
 
